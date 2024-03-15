ec9b990d9db4f47dea708798b219bdca6b81717a
oulevren <evrenogul20@gmail.com>
2024-03-15 14:50:56 +0300

    login register,profile add ,update,delete

--- netflix/netflix_app/views.py
+++ netflix/netflix_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
 def index(request):
+
+    if request.user.is_authenticated:
+        return redirect("page-select-profile")
+
     return render(request,"index.html")
--- netflix/user_app/urls.py
+++ netflix/user_app/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from netflix.urls import *
 from .views import *
 
+#config
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
-    path('register',register_page,name="register-page"),
-    path('login',login_page,name="login-page")
-]
+    path('register',register_page,name="page-register"),
+    path('login',login_page,name="page-login"),
+    path('browse/profile',select_profile,name="page-select-profile"),
+    path('browse/profiles/<profileId>/update',updateProfile,name="page-update-profile"),
+    path('browse/profiles/<profileId>/delete',delete_profile,name="page-delete-profile")
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
